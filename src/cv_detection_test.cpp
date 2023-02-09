c642907879d18e6cd7fdb3f714b74c9ca41ad26d
yifanyin11 <yifanyin211@gmail.com>
2023-02-08 22:30:19 -0500

    color seg

--- src/cv_detection_test.cpp
+++ src/cv_detection_test.cpp
@@ -19,39 +19,36 @@ int main(int argc, char** argv){
     // detection setups
     visual_servo::ImageCapturer cam1(nh, img_topic1);
     // visual_servo::ImageCapturer cam2(nh, img_topic2);
-    visual_servo::ToolDetector detector_toolcenter(nh, std::vector<int>{150, 150, 150, 160, 255, 255});
-    visual_servo::ToolDetector detector_tooltip(nh, std::vector<int>{20, 100, 100, 30, 255, 255});
-    // visual_servo::ToolDetector detector_blue(nh, std::vector<int>{111, 60, 70, 117, 80, 110});
-    visual_servo::ToolDetector detector_blue(nh, std::vector<int>{115, 60, 80, 118, 80, 100});
 
-    visual_servo::ToolDetector detector_red(nh, std::vector<int>{0, 100, 100, 5, 255, 255});
-    visual_servo::ToolDetector detector_target_tip1(nh, std::vector<int>{130, 30, 30, 140, 255, 120});
-    visual_servo::ToolDetector detector_target_tip2(nh, std::vector<int>{76, 100, 150, 96, 255, 255});
+    visual_servo::ToolDetector detector_blue(nh, std::vector<int>{115, 60, 80, 118, 80, 100});
+    visual_servo::ToolDetector detector_red(nh, std::vector<int>{0, 120, 110, 5, 160, 190});
+    // visual_servo::ToolDetector detector_green(nh, std::vector<int>{60, 30, 100, 80, 60, 130});
+    // visual_servo::ToolDetector detector_brown(nh, std::vector<int>{5, 70, 60, 10, 90, 80});
+    visual_servo::ToolDetector detector_orange(nh, std::vector<int>{5, 120, 160, 10, 140, 170});
+    visual_servo::ToolDetector detector_pink(nh, std::vector<int>{165, 120, 130, 175, 135, 150});
 
     int count = 0;
+    detector_pink.firstDetect(cam1);
+    while(nh.ok()){ 
+        count++;
+        detector_pink.track(cam1, 0.25, 0.25);
+        cam1.saveCurrentImage("./", std::to_string(count)+".png");
+        detector_pink.drawDetectRes();
 
-    
+        // detector_target.detect(cam2);
+        // detector_target.drawDetectRes();
 
-    // while(nh.ok()){
-    //     count++;
-    //     detector_blue.detect(cam1);
-    //     cam1.saveCurrentImage("./", std::to_string(count)+".png");
-    //     detector_blue.drawDetectRes();
+        // detector_target_tip1.detect(cam1);
+        // detector_target_tip1.drawDetectRes();
+        // detector_target_tip1.detect(cam2);
+        // detector_target_tip1.drawDetectRes();
 
-    //     // detector_target.detect(cam2);
-    //     // detector_target.drawDetectRes();
-
-    //     // detector_target_tip1.detect(cam1);
-    //     // detector_target_tip1.drawDetectRes();
-    //     // detector_target_tip1.detect(cam2);
-    //     // detector_target_tip1.drawDetectRes();
-
-    //     // detector_target_tip2.detect(cam1);
-    //     // detector_target_tip2.drawDetectRes();
-    //     // detector_target_tip2.detect(cam2);
-    //     // detector_target_tip2.drawDetectRes();
-    //     rate.sleep();
-    // }
+        // detector_target_tip2.detect(cam1);
+        // detector_target_tip2.drawDetectRes();
+        // detector_target_tip2.detect(cam2);
+        // detector_target_tip2.drawDetectRes();
+        rate.sleep();
+    }
 
     ros::shutdown();
     return 0;
--- src/tool_detector.cpp
+++ src/tool_detector.cpp
@@ -103,7 +103,7 @@ void visual_servo::ToolDetector::detect(ImageCapturer& cam){
 
     cv::reduce(mask, col_sum, 0, cv::REDUCE_SUM, CV_64FC1); 
     cv::reduce(mask, row_sum, 1, cv::REDUCE_SUM, CV_64FC1); 
-    int start1, end1, start2, end2;
+    int start1=-1, end1=-1, start2=-1, end2=-1;
     for (int i=0; i<col_sum.size[1]; ++i){
         if (col_sum.at<double>(0,i)!=0){
             start1 = i;
@@ -129,12 +129,14 @@ void visual_servo::ToolDetector::detect(ImageCapturer& cam){
             break;
         }
     }
-    tool_center.x = (start1+end1)/2.0;
-    tool_center.y = (start2+end2)/2.0;
-    corner1.x = start1;
-    corner1.y = end2;
-    corner2.x = end1;
-    corner2.y = start2;
+    if(start1<image.cols && start2<image.rows && end1<image.cols && end2<image.rows){
+        tool_center.x = (start1+end1)/2.0;
+        tool_center.y = (start2+end2)/2.0;
+        corner1.x = start1;
+        corner1.y = end2;
+        corner2.x = end1;
+        corner2.y = start2;
+    }
 
     while(nh.ok()){
         ros::spinOnce();
@@ -156,7 +158,7 @@ void visual_servo::ToolDetector::detect(cv::Mat& img){
 
     cv::reduce(mask, col_sum, 0, cv::REDUCE_SUM, CV_64FC1); 
     cv::reduce(mask, row_sum, 1, cv::REDUCE_SUM, CV_64FC1); 
-    int start1, end1, start2, end2;
+    int start1=-1, end1=-1, start2=-1, end2=-1;
     for (int i=0; i<col_sum.size[1]; ++i){
         if (col_sum.at<double>(0,i)!=0){
             start1 = i;
@@ -182,12 +184,14 @@ void visual_servo::ToolDetector::detect(cv::Mat& img){
             break;
         }
     }
-    tool_center.x = (start1+end1)/2.0;
-    tool_center.y = (start2+end2)/2.0;
-    corner1.x = start1;
-    corner1.y = end2;
-    corner2.x = end1;
-    corner2.y = start2;
+    if(start1<img.cols && start2<img.rows && end1<img.cols && end2<img.rows){
+        tool_center.x = (start1+end1)/2.0;
+        tool_center.y = (start2+end2)/2.0;
+        corner1.x = start1;
+        corner1.y = end2;
+        corner2.x = end1;
+        corner2.y = start2;
+    }
 
     while(nh.ok()){
         ros::spinOnce();
@@ -200,14 +204,27 @@ void visual_servo::ToolDetector::track(ImageCapturer& cam, double roir_width=0.2
     image = cam.getCurrentImage();
     // create a img with a fixed size | dim constraints
     int h = image.rows, w = image.cols;
-    int roi_w = w*roir_width, roi_h = h*roir_height;
-    cv::Point roi_origin(std::max(tool_center.x-roi_w/2,0), std::max(tool_center.y-roi_h/2,0));
-    cv::Mat roi = image(cv::Range(roi_origin.x, std::min(tool_center.x+roi_w/2,w)), 
-        cv::Range(roi_origin.y, std::min(tool_center.y+roi_h/2,h)));
-    // detect inside roi
-    visual_servo::ToolDetector::detect(roi);
-    tool_center.x+=roi_origin.x;
-    tool_center.y+=roi_origin.y;
+    if (tool_center.x==-1 || tool_center.y==-1 || tool_center.x>=w || tool_center.y>=h){
+        while (tool_center.x==-1 || tool_center.y==-1 || tool_center.x>=image.cols || tool_center.y>=image.rows){
+            ROS_ERROR("TRYING TO CORRECT AFTER DETECTION FAILED ...");
+            image = cam.getCurrentImage();
+            visual_servo::ToolDetector::detect(image);
+        }
+    }
+    else{
+        int roi_w = w*roir_width, roi_h = h*roir_height;
+        cv::Point roi_origin(int(std::max(tool_center.x-roi_w/2,0)), int(std::max(tool_center.y-roi_h/2,0)));
+        cv::Mat roi = image(cv::Range(roi_origin.y, int(std::min(tool_center.y+roi_h/2,h))), 
+            cv::Range(roi_origin.x, int(std::min(tool_center.x+roi_w/2,w))));
+        // detect inside roi
+        visual_servo::ToolDetector::detect(roi);
+        tool_center.x+=roi_origin.x;
+        tool_center.y+=roi_origin.y;
+        corner1.x+=roi_origin.x;
+        corner1.y+=roi_origin.y;
+        corner2.x+=roi_origin.x;
+        corner2.y+=roi_origin.y;
+    }
 }
 
 void visual_servo::ToolDetector::drawDetectRes(){
