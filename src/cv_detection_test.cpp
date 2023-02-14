929d0e4610c20ffdbec80eb41b0a8119109ae6ee
yifanyin11 <yifanyin211@gmail.com>
2023-02-13 21:35:01 -0500

    working version of color segmentation

--- src/cv_detection_test.cpp
+++ src/cv_detection_test.cpp
@@ -20,33 +20,21 @@ int main(int argc, char** argv){
     visual_servo::ImageCapturer cam1(nh, img_topic1);
     // visual_servo::ImageCapturer cam2(nh, img_topic2);
 
-    visual_servo::ToolDetector detector_blue(nh, std::vector<int>{115, 60, 80, 118, 80, 100});
-    visual_servo::ToolDetector detector_red(nh, std::vector<int>{0, 120, 110, 5, 160, 190});
+    visual_servo::ToolDetector detector_blue(nh, std::vector<int>{100, 130, 75, 107, 150, 90});
+    visual_servo::ToolDetector detector_red(nh, std::vector<int>{0, 160, 100, 5, 170, 120});
     // visual_servo::ToolDetector detector_green(nh, std::vector<int>{60, 30, 100, 80, 60, 130});
     // visual_servo::ToolDetector detector_brown(nh, std::vector<int>{5, 70, 60, 10, 90, 80});
     visual_servo::ToolDetector detector_orange(nh, std::vector<int>{5, 120, 160, 10, 140, 170});
+    visual_servo::ToolDetector detector_purple(nh, std::vector<int>{140, 80, 60, 150, 100, 75});
     visual_servo::ToolDetector detector_pink(nh, std::vector<int>{165, 120, 130, 175, 135, 150});
 
     int count = 0;
-    detector_pink.firstDetect(cam1);
+    detector_blue.firstDetect(cam1);
     while(nh.ok()){ 
         count++;
-        detector_pink.track(cam1, 0.25, 0.25);
+        detector_blue.track(cam1, 0.25, 0.25);
         cam1.saveCurrentImage("./", std::to_string(count)+".png");
-        detector_pink.drawDetectRes();
-
-        // detector_target.detect(cam2);
-        // detector_target.drawDetectRes();
-
-        // detector_target_tip1.detect(cam1);
-        // detector_target_tip1.drawDetectRes();
-        // detector_target_tip1.detect(cam2);
-        // detector_target_tip1.drawDetectRes();
-
-        // detector_target_tip2.detect(cam1);
-        // detector_target_tip2.drawDetectRes();
-        // detector_target_tip2.detect(cam2);
-        // detector_target_tip2.drawDetectRes();
+        detector_blue.drawDetectRes();
         rate.sleep();
     }
 
